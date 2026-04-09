6b3de7d4da147dee753eda9bcf7fec6abdcf61ff
jiancaiFan <121850483+jiancaiFan@users.noreply.github.com>
2026-04-09 23:38:47 +0800

    Updated homeuimodel init.

--- composeApp/src/commonMain/kotlin/cn/heartbath/mambo_lear_hub/manbolearhub/model/home/HomeUIModel.kt
+++ composeApp/src/commonMain/kotlin/cn/heartbath/mambo_lear_hub/manbolearhub/model/home/HomeUIModel.kt
@@ -3,9 +3,9 @@ package cn.heartbath.mambo_lear_hub.manbolearhub.model.home
 import cn.heartbath.mambo_lear_hub.manbolearhub.ui.home.CategoryItem
 
 data class HomeUIModel(
-    val categories: List<CategoryItem> = emptyList(),
-    val selectedCategory: Int = 0,
-    val categoryDataMap: Map<Int, List<String>> = emptyMap()
+    val categories: List<CategoryItem>,
+    val selectedCategory: Int,
+    val categoryDataMap: Map<Int, List<String>>
 ) {
     companion object {
         val Empty = HomeUIModel(
