0cb632f42c7570ec64bf4be43d9d3c0468738d79
jiancaiFan <121850483+jiancaiFan@users.noreply.github.com>
2026-04-09 13:47:34 +0800

    Updated homesideeffect

--- composeApp/src/commonMain/kotlin/cn/heartbath/mambo_lear_hub/manbolearhub/redux/home/HomeSideEffect.kt
+++ composeApp/src/commonMain/kotlin/cn/heartbath/mambo_lear_hub/manbolearhub/redux/home/HomeSideEffect.kt
@@ -3,11 +3,15 @@ package cn.heartbath.mambo_lear_hub.manbolearhub.redux.home
 import cn.heartbath.mambo_lear_hub.manbolearhub.repository.home.HomeRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.reduxkotlin.Middleware
 import org.reduxkotlin.Store
+import kotlin.time.Duration.Companion.milliseconds
 
 object HomeSideEffect {
+
+    private const val MIN_LOADING_MS = 400L
 
     fun createHomeMiddleware(
         repository: HomeRepository,
@@ -36,6 +40,9 @@ object HomeSideEffect {
                         store.dispatch(HomeAction.LoadCategoryStarted(p))
 
                         scope.launch {
+                            // 最小展示时长：先等 400ms，再请求（开发调试最稳）
+                            delay(MIN_LOADING_MS.milliseconds)
+
                             runCatching { repository.fetchCategoryData(p) }
                                 .onSuccess { data ->
                                     store.dispatch(HomeAction.LoadCategorySucceeded(p, data))
