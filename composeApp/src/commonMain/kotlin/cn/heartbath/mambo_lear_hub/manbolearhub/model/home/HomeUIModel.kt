8bd161e91851368f4e0c1d330b953a7f1bbd1260
jiancaiFan <121850483+jiancaiFan@users.noreply.github.com>
2026-04-16 16:15:38 +0800

    Fix uimodelL import issue

--- composeApp/src/commonMain/kotlin/cn/heartbath/mambo_lear_hub/manbolearhub/model/home/HomeUIModel.kt
+++ composeApp/src/commonMain/kotlin/cn/heartbath/mambo_lear_hub/manbolearhub/model/home/HomeUIModel.kt
@@ -1,7 +1,5 @@
 package cn.heartbath.mambo_lear_hub.manbolearhub.model.home
 
-import cn.heartbath.mambo_lear_hub.manbolearhub.model.forumdetail.HomeUIModel
-
 data class HomeUIModel(
     val categories: List<CategoryItem>,
     val selectedCategory: Int,
