f6c54b2dd19d0c619db85dc5db61c032495581ba
jiancaiFan <121850483+jiancaiFan@users.noreply.github.com>
2026-04-16 16:37:45 +0800

    Updated di

--- composeApp/src/commonMain/kotlin/cn/heartbath/mambo_lear_hub/manbolearhub/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/cn/heartbath/mambo_lear_hub/manbolearhub/di/AppModule.kt
@@ -3,11 +3,13 @@ package cn.heartbath.mambo_lear_hub.manbolearhub.di
 import cn.heartbath.mambo_lear_hub.manbolearhub.network.KtorNetworkClient
 import cn.heartbath.mambo_lear_hub.manbolearhub.network.NetworkClient
 import cn.heartbath.mambo_lear_hub.manbolearhub.network.createKtorRawClient
+import cn.heartbath.mambo_lear_hub.manbolearhub.redux.forumdetail.ForumDetailRepositoryImpl
 import cn.heartbath.mambo_lear_hub.manbolearhub.redux.forumdetail.ForumDetailState
 import cn.heartbath.mambo_lear_hub.manbolearhub.redux.forumdetail.ForumDetailStoreProvider
 import cn.heartbath.mambo_lear_hub.manbolearhub.redux.home.HomeRepositoryImpl
 import cn.heartbath.mambo_lear_hub.manbolearhub.redux.home.HomeState
 import cn.heartbath.mambo_lear_hub.manbolearhub.redux.home.HomeStoreProvider
+import cn.heartbath.mambo_lear_hub.manbolearhub.repository.forumdetail.ForumDetailRepository
 import cn.heartbath.mambo_lear_hub.manbolearhub.repository.home.HomeRepository
 import cn.heartbath.mambo_lear_hub.manbolearhub.viewmodel.ManBoLearHubViewModel
 import cn.heartbath.mambo_lear_hub.manbolearhub.viewmodel.forumdetail.ForumDetailViewModel
@@ -35,6 +37,13 @@ val appModule = module {
 
     single<HomeRepository> {
         HomeRepositoryImpl(
+            networkClient = get(),
+            baseUrl = BASE_URL
+        )
+    }
+
+    single<ForumDetailRepository> {
+        ForumDetailRepositoryImpl(
             networkClient = get(),
             baseUrl = BASE_URL
         )
